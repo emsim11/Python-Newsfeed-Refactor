06d68132e0d6204f4d0bf5eb319313ab3a8558b6
emsim11 <emsimone11@gmail.com>
2024-07-16 21:11:52 -0400

    Create Filter Functions & Add To Template Files

---
+++ App/Utilities/Filters.py
@@ -0,0 +1,36 @@
+# Format Dates As Mon Jan 31, 2022 at 11:59PM
+# More Options: https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
+def Format_Date(Date):
+  # Receive a `datetime` Object
+  # Use `strftime()` Method To Convert To String
+  return Date.strftime('%a %b %d, %Y at %I:%M%p')
+
+# Remove Extraneous Info From URL String
+def Format_URL(URL):
+  # Only Keep Domain Name (Remove Query Parameters)
+  return URL.replace('http://', '').replace('https://', '').replace('www.', '').split('/')[0].split('?')[0]
+
+# Correctly Pluralize Words
+def Format_Plural(Amount, Word):
+  # If Amount Is Greater Than 1, Pluralize
+  if Amount != 1:
+    return Word + 's'
+  # Otherwise, Return The Word Unaltered
+  return Word
+
+# 🧪 TEST: `python App/Utilities/Filters.py`
+# ➡️ RETURNS: `Tue Jul 16, 2024 at 8:20PM` ☑️
+from datetime import datetime
+print(Format_Date(datetime.now()))
+
+# 🧪 TEST: `python App/Utilities/Filters.py`
+# ➡️ RETURNS: `google.com` ☑️
+print(Format_URL('http://google.com/test/'))
+# ➡️ RETURNS: `google.com` ☑️
+print(Format_URL('https://www.google.com?q=test'))
+
+# 🧪 TEST: `python App/Utilities/Filters.py`
+# ➡️ RETURNS: `Cats` ☑️
+print(Format_Plural(2, 'Cat'))
+# ➡️ RETURNS: `Dog` ☑️
+print(Format_Plural(1, 'Dog'))
--- App/__Init__.py
+++ App/__Init__.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from App.Routes import Home, Dashboard
 from App.Database import Init_Database
+from App.Utilities import Filters
 
 def create_app(test_config=None):
   # Setup App Configuration
@@ -20,6 +21,11 @@ def create_app(test_config=None):
   # Register Routes
   App.register_blueprint(Home)
   App.register_blueprint(Dashboard)
+  
+  # Register Custom Filter Functions
+  App.jinja_env.filters['Format_URL'] = Filters.Format_URL
+  App.jinja_env.filters['Format_Date'] = Filters.Format_Date
+  App.jinja_env.filters['Format_Plural'] = Filters.Format_Plural
   
   # Setup Database Connection In Flask App
   Init_Database(App)
